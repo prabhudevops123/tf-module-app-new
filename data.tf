7abada7a30802d6d186f6b14e1465a41b9bf8eea
Prabhu <prabh.rao@gmail.com>
2023-06-21 19:17:17 +0530

    Prometheus

--- data.tf
+++ data.tf
@@ -1,5 +1,6 @@
 data "aws_ami" "ami" {
-  owners      = ["833689273858"]
+ # owners      = ["833689273858"]
+  owners      = ["973714476881"]
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
 }
